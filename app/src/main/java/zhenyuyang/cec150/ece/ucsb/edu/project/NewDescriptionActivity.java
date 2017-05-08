405b72ecdd6e3530334d10fee5125f42e819b1e3
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-05-07 17:40:01 -0700

    add NewDescriptionActivity

--- app/src/main/java/zhenyuyang/cec150/ece/ucsb/edu/project/NewDescriptionActivity.java
+++ app/src/main/java/zhenyuyang/cec150/ece/ucsb/edu/project/NewDescriptionActivity.java
@@ -1,7 +1,13 @@
 package zhenyuyang.cec150.ece.ucsb.edu.project;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewDescriptionActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class NewDescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_description);
+
+        //add button
+        Button button = (Button) findViewById(R.id.button_new_description);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent returnIntent = new Intent();
+                EditText ev = (EditText) findViewById(R.id.editText_new_description);
+                    returnIntent.putExtra("result", ev.getText().toString());
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
+
+
+                //if don't want to return data:
+//                Intent returnIntent = new Intent();
+//                setResult(Activity.RESULT_CANCELED, returnIntent);
+//                finish();
+
+            }
+
+        });
+
     }
 }
--- app/src/main/java/zhenyuyang/cec150/ece/ucsb/edu/project/newTown.java
+++ app/src/main/java/zhenyuyang/cec150/ece/ucsb/edu/project/newTown.java
@@ -28,7 +28,8 @@ public class newTown extends AppCompatActivity {
     final int NEW_TITLE_REQUEST = 0;
     final int NEW_ADDRESS_REQUEST = 1;
     final int NEW_CATEGORY_REQUEST = 2;
-    final int NEW_INFORMATION_REQUEST =3;
+    final int NEW_DESCRIPTION_REQUEST = 3;
+    final int NEW_INFORMATION_REQUEST =4;
 
     String title = "";
 
@@ -117,6 +118,14 @@ public class newTown extends AppCompatActivity {
                     //finish();// kill current activity
                 }
 
+                if (position == 3) {
+                    Intent intent = new Intent(getApplicationContext(), NewDescriptionActivity.class);
+                    //intent.putExtra(EXTRA_MESSAGE, "asdf");
+                    startActivityForResult(intent, NEW_DESCRIPTION_REQUEST);
+                    overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
+                    //finish();// kill current activity
+                }
+
 
 
             }
@@ -168,6 +177,19 @@ public class newTown extends AppCompatActivity {
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 Log.i("onActivityResult", "NEW_CATEGORY_REQUEST RESULT_CANCELED");
+                //Write your code if there's no result
+            }
+        }
+
+        if (requestCode == NEW_DESCRIPTION_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("result");
+                Log.i("onActivityResult", "result = " + result);
+                title = result;
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("onActivityResult", "NEW_DESCRIPTION_REQUEST RESULT_CANCELED");
                 //Write your code if there's no result
             }
         }
